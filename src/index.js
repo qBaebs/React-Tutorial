3319e8fc215e014ad944cd1f15e95fea6f5d4c8a
Quinn Battle <quinn.l.battle@gmail.com>
2023-01-27 02:36:51 -0600

    Added constructor to board class
    states squares array with 9 nulls

--- src/index.js
+++ src/index.js
@@ -20,6 +20,14 @@ this.state = { value: null,};
   }
   
   class Board extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
     renderSquare(i) {
       return <Square value={i} />;
     }
