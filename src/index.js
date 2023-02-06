1f0bd5564c847428d770988c050cfef07b587fa2
Quinn Battle <quinn.l.battle@gmail.com>
2023-02-05 23:12:37 -0600

    Changed Square class to be a function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -24,10 +19,10 @@ class Board extends React.Component {
     };
   }
 
-  handleClick(i){
+  handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({ squares: squares });
   }
 
   renderSquare(i) {
